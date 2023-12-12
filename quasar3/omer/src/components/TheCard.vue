5cac38f471d678cddef372597f68f2f74592c76f
Omer Gun <omrgnts61@gmail.com>
2023-12-13 00:26:46 +0300

    Card Component

---
+++ quasar3/omer/src/components/TheCard.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+      <q-card style="width: 100%;
+    max-width: 450px" flat bordered>
+        <div class="bg-black">
+          <q-tooltip  class="text-body1 bg-grey-2" anchor="top right" self="top end" :offset="[-20, -25]">
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/></svg>
+            </q-tooltip>
+          <q-tooltip class="text-body1" style="max-height:20px; max-width:20px" anchor="bottom middle" self="center middle" :offset="[-200, -25]">
+    Hızlı İncele!
+  </q-tooltip>
+       <a href="#"> <q-img
+          src="https://8f08a8.cdn.akinoncloud.com/products/2023/05/08/3006322/33b8eef9-bd8e-412a-82a0-5aecefd4b20a_size2010x2010_cropCenter.jpg"
+                           ></q-img></a>
+        </div>
+
+        <q-card-section>
+
+          <div class="text-h6 q-pa-md ">New Balance 650 R</div>
+          <div class=" q-pa-md text-caption text-grey">
+            Günlük Giyim
+          </div>
+          <h5 style="padding: 0; margin: 0 ;text-align: right;
+    line-height: 50px;
+    top: 5px">4.424,25₺</h5>
+        </q-card-section>
+
+      </q-card>
+
+    <q-card style="width: 100%;
+    max-width: 450px" flat bordered>
+        <div class="bg-black">
+          <q-tooltip  class="text-body1 bg-grey-2" anchor="top right" self="top end" :offset="[-20, -25]">
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/></svg>
+            </q-tooltip>
+          <q-tooltip class="text-body1" style="max-height:20px; max-width:20px" anchor="bottom middle" self="center middle" :offset="[-200, -25]">
+    Hızlı İncele!
+  </q-tooltip>
+       <a href="#"> <q-img
+          src="https://8f08a8.cdn.akinoncloud.com/products/2023/05/08/3005477/3767b845-33be-4a22-8c13-acb2a9494441_size2010x2010_cropCenter.jpg"
+                           ></q-img></a>
+        </div>
+
+        <q-card-section>
+
+          <div class="text-h6 q-pa-md ">New Balance 650 B</div>
+          <div class=" q-pa-md text-caption text-grey">
+            Günlük Giyim
+          </div>
+          <h5 style="padding: 0; margin: 0 ;text-align: right;
+    line-height: 50px;
+    top: 5px">5.424,25₺</h5>
+        </q-card-section>
+
+      </q-card>
+
+    <q-card style="width: 100%;
+    max-width: 450px" flat bordered>
+        <div class="bg-black">
+          <q-tooltip  class="text-body1 bg-grey-2" anchor="top right" self="top end" :offset="[-20, -25]">
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/></svg>
+            </q-tooltip>
+          <q-tooltip class="text-body1" style="max-height:20px; max-width:20px" anchor="bottom middle" self="center middle" :offset="[-200, -25]">
+    Hızlı İncele!
+  </q-tooltip>
+       <a href="#"> <q-img
+          src="https://8f08a8.cdn.akinoncloud.com/products/2023/11/21/16990426/5c53b4db-4a7d-4e4f-87ba-6691dd3a3315_size2010x2010_cropCenter.jpg"
+                           ></q-img></a>
+        </div>
+
+        <q-card-section>
+
+          <div class="text-h6 q-pa-md ">New Balance 650</div>
+          <div class=" q-pa-md text-caption text-grey">
+            Günlük Giyim
+          </div>
+          <h5 style="padding: 0; margin: 0 ;text-align: right;
+    line-height: 50px;
+    top: 5px">2.754,25₺</h5>
+        </q-card-section>
+
+
+
+      </q-card>
+
+      <q-card v-ripple style="width: 100%;
+    max-width: 450px" flat bordered>
+        <div class="bg-black q-hoverable cursor-pointer">
+          <q-tooltip  class="text-body1 bg-grey-2" anchor="top right" self="top end" :offset="[-20, -25]">
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/></svg>
+            </q-tooltip>
+          <q-tooltip class="text-body1" style="max-height:20px; max-width:20px" anchor="bottom middle" self="center middle" :offset="[-200, -25]">
+    Hızlı İncele!
+  </q-tooltip>
+       <a href="#"> <q-img
+          src="https://8f08a8.cdn.akinoncloud.com/products/2023/11/21/16990426/5c53b4db-4a7d-4e4f-87ba-6691dd3a3315_size2010x2010_cropCenter.jpg"
+                           ></q-img></a>
+        </div>
+   <span class="q-focus-helper"></span>
+        <q-card-section>
+
+          <div class="text-h6 q-pa-md ">New Balance 650</div>
+          <div class=" q-pa-md text-caption text-grey">
+            Günlük Giyim
+          </div>
+          <h5 style="padding: 0; margin: 0 ;text-align: right;
+    line-height: 50px;
+    top: 5px">2.754,25₺</h5>
+        </q-card-section>
+
+
+
+      </q-card>
+
+
+
+    </div>
+  </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      expanded: ref(false),
+    }
+  }
+}
+</script>
