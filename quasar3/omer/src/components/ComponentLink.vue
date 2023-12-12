2ea8c2afa5b1d5f95cb6484d2ae82212ec778cb8
Omer Gun <omrgnts61@gmail.com>
2023-12-13 00:22:27 +0300

    Component Klasörü

---
+++ quasar3/omer/src/components/ComponentLink.vue
@@ -0,0 +1,51 @@
+<template>
+  <router-link :to="link">
+  <q-item
+
+    tag="a"
+    target="_blank"
+
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ComponentLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    caption: {
+      type: String,
+      default: ''
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+});
+</script>
